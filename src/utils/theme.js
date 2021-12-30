b30b2aa76780378d65fae5cd1ecdb4af80523dfa
Chris Diorio <chris.diorio12@gmail.com>
2021-12-30 11:53:19 -0600

    Forgot to export theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,4 +1,4 @@
-const Theme = {
+const theme = {
   colors: {
     main: "#003459",
     mainDark: "#00171F",
@@ -9,10 +9,12 @@ const Theme = {
     shadow: "rgba(0,0,0,.2)",
   },
   mediaQueries: {
-      smallest: `only screen and (max-width: 25em)`,
-      small: `only screen and (max-width: 37.5em)`,
-      medium: `only screen and (max-width: 56em)`,
-      large: `only screen and (max-width: 80em)`,
-      largest: `only screen and (max-width: 90em)`,
-  }
+    smallest: `only screen and (max-width: 25em)`,
+    small: `only screen and (max-width: 37.5em)`,
+    medium: `only screen and (max-width: 56em)`,
+    large: `only screen and (max-width: 80em)`,
+    largest: `only screen and (max-width: 90em)`,
+  },
 };
+
+export default theme;
